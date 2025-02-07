b2f42087d45f178065ec497fe4bdd345e71b886f
netherfasterthanyou <adityakkaipa@gmail.com>
2025-02-06 18:44:41 -0800

    Initial commit

---
+++ src/main/java/com/more_of_all/MoreOfAllDataGenerator.java
@@ -0,0 +1,11 @@
+package com.more_of_all;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreOfAllDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
